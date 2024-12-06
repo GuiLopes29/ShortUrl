c3708507b46b422945600199f7adb1249a3824e2
Guilherme Lopes <guiplopes29@gmail.com>
2024-12-06 14:36:22 -0300

    feat: update to ec2

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -6,15 +6,12 @@ dotenvConfig({ path: '.env' });
 
 const config = {
   type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT, 10),
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  url: process.env.DATABASE_URL,
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
   autoLoadEntities: true,
   synchronize: true,
+  ssl: { rejectUnauthorized: false },
 };
 
 export default registerAs('typeorm', () => config);
--- src/data-source.ts
+++ src/data-source.ts
@@ -5,12 +5,9 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT, 10),
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  url: process.env.DATABASE_URL,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/../migrations/*.ts'],
   synchronize: true,
+  ssl: { rejectUnauthorized: false },
 });
